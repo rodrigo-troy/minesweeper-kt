6bd153a003403618a8abdd79cd025ced3769b94a
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-04-02 20:45:51 -0400

    Stage 3

--- Minesweeper (Kotlin)/task/src/minesweeper/Main.kt
+++ Minesweeper (Kotlin)/task/src/minesweeper/Main.kt
@@ -4,8 +4,9 @@ fun main() {
     println("How many mines do you want on the field?")
     val mines = readln().toInt()
 
-    val field = Field(9, 9, mines)
+    val field = Field(9,
+                      9,
+                      mines)
+
     field.printField()
-
-
 }
